eef91dbc9d16497913efed9b625e7eec07a8a8b2
Stingo36 <sahiltari36@gmail.com>
2025-11-16 10:31:27 +0530

    1

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -11,4 +11,17 @@ $databases['default']['default'] = array (
   'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
   'driver' => 'mysql',
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);$databases['default']['default'] = array (
+  'database' => 'Buildkit',
+  'username' => 'admin',
+  'password' => 'admin',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 );
+$settings['hash_salt'] = 'u23Q61M_a4tRm6bFYdbucmgxYVH0ZUfDMdRu9VkeL1mPxKRyEpotTONlk0hDPv0fyq3L-ir4QQ';
+$settings['config_sync_directory'] = 'profiles/custom/stingo_buildkit/config/sync';
